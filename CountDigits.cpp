796d4c3588b2cd831ed586ba3103b4e46e34bbf5
MayankDhingra <dhingramayank82@gmail.com>
2026-04-30 23:55:58 +0530

    CountDigits

---
+++ CountDigits.cpp
@@ -0,0 +1,6 @@
+# include <bits/stdc++.h>
+using namespace std; 
+int main(){
+    int n = 2435 ;
+    cout << (int)(log10(n) + 1) << endl ;
+}
--- DigitsInANumber.cpp
+++ DigitsInANumber.cpp
@@ -1,6 +1,7 @@
 // base 10 number take modulo 10 and then divide it by ten
 # include <bits/stdc++.h>
 using namespace std ;
+// TC - O(log n base 10) as number is getting divide by 10  if numner is getting divide by 3 then tc will be log n base 3
 void solve(int n){
     while(n!=0){
         int rem = n%10 ;
