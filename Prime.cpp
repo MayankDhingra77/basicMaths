f88439e245ab5b434e29f2c792d7be20d85bbb4a
MayankDhingra <dhingramayank82@gmail.com>
2026-04-30 23:46:38 +0530

    updated with seive of eratosthenes

--- Prime.cpp
+++ Prime.cpp
@@ -4,14 +4,22 @@
 // Isliye √n tak check karna enough hai 
 # include <bits/stdc++.h>
 using namespace std ;
+// bool isPrime(int n){
+//     if(n== 0 ||  n==1) return false ;
+//     if(n==2 ) return true ;
+//     if(n%2 ==0) return false ;
+//     for(int i = 3 ; i*i < n ; i+=2){
+//         if(n%i == 0) return false ;
+//     }
+//     return true ;
+// }
+
+
+//Count Primes
+//Sieve of Eratosthenes - assume all nummbers as prime remove all multiples of 2 and 3 and 5 and 7 
 bool isPrime(int n){
-    if(n== 0 ||  n==1) return false ;
-    if(n==2 ) return true ;
-    if(n%2 ==0) return false ;
-    for(int i = 3 ; i*i < n ; i+=2){
-        if(n%i == 0) return false ;
-    }
-    return true ;
+    if(n<=1) return false ;
+
 }
 int main(){
     int n = 42 ;
