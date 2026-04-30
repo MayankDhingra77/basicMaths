ad95760e15c0792f44d29373b1fb425d1564ec56
MayankDhingra <dhingramayank82@gmail.com>
2026-04-30 23:34:52 +0530

    prime

---
+++ Prime.cpp
@@ -0,0 +1,20 @@
+// Short summary - to find prime no
+// Factors pair me aate hain → (a, b)
+// Ek factor hamesha √n se chhota ya equal hota hai
+// Isliye √n tak check karna enough hai 
+# include <bits/stdc++.h>
+using namespace std ;
+bool isPrime(int n){
+    if(n== 0 ||  n==1) return false ;
+    if(n==2 ) return true ;
+    if(n%2 ==0) return false ;
+    for(int i = 3 ; i*i < n ; i+=2){
+        if(n%i == 0) return false ;
+    }
+    return true ;
+}
+int main(){
+    int n = 42 ;
+    cout << isPrime(n) ;
+    return 0 ;
+}
