54bb23d0dd2272c34ffc0538dd53223cb30864ca
MayankDhingra <dhingramayank82@gmail.com>
2026-05-01 18:54:24 +0530

    modulo

---
+++ modulo.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // (((x%m)%m)%m).. will always result in x%m 
+
+    return 0;
+}
